e9c4a83a330de8b04aac7b962970ca548bd4d91a
张会亮 <crazyhuiliang@163.com>
2018-02-07 09:34:11 +0800

    rename

---
+++ 14-Iterator和for-of循环/01-makeIterator.js
@@ -0,0 +1,17 @@
+// 模仿next 方法返回值
+function makeIterator(array) {
+    var nextIndex = 0
+    return {
+        next: function () {
+            return nextIndex < array.length ?
+                {value: array[nextIndex++], done: false}:
+                {value: undefined, done:true}
+        }
+    }
+}
+
+var it = makeIterator(['a', 'b'])
+
+console.log(it.next())
+console.log(it.next())
+console.log(it.next())
